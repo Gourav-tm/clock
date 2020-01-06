385329c7b42b2c7af2ae142ecd5bd8f3597c902c
gourav-tm <gouravj@teramatrix.co>
2020-01-06 18:08:03 +0530

    Create Clock Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.showTime = this.showTime.bind(this);
+    this.state = {
+      time: ""
+    }
+  }
+  componentDidMount() {
+    this.showTime();
+  }
+  showTime() {
+    const _this = this;
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if (h === 0) {
+      h = 12;
+    }
+
+    if (h > 12) {
+      h = h - 12;
+      session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    this.setState({
+      time: time
+    })
+    setTimeout(_this.showTime, 1000);
+
+  }
+  render() {
+    return (
+      <div>
+        <div className="clock" >{this.state.time}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
